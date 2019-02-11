f7d8fd2cbbf6102e2f543b665210da8c68e771b8
philippzach <mail@philippzach.com>
2019-02-11 10:32:51 -1000

    changed to Factory  prismic repositroy

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-prismic',
       options: {
-        repositoryName: 'swiss-startup-group',
+        repositoryName: 'swiss-startup-factory',
         accessToken: `${process.env.API_KEY}`,
         linkResolver: () => post => `/${post.uid}`,
         htmlSerializer: () => (type, element, content) => {
